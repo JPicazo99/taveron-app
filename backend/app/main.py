8c7579184e260a66d4f240cd1ed65fddd770169d
JPicazo <joel.picazo@cinvestav.mx>
2025-08-10 12:23:41 -0600

    Fixing main and requirements

--- backend/app/main.py
+++ backend/app/main.py
@@ -18,8 +18,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        # agrega aquí tu dominio de Vercel cuando despliegues
-        # "https://tu-frontend.vercel.app",
+        "https://taveron-app.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
